c1d07b33d8225b02efaf4a50143e6892d50c7807
iraqez <vladimir.gumenyuk@gmail.com>
2016-07-06 18:47:56 +0300

    f

--- datacadnum.py
+++ datacadnum.py
@@ -1,27 +1,25 @@
 import json, requests
+import coordinates
 url_point = 'http://212.26.144.110/kadastrova-karta/find-Parcel'
 url_data = 'http://212.26.144.110/kadastrova-karta/get-parcel-Info'
 
-#cadnum='6810100000:07:003:0064'
-cadnum = input('Вставьте кадастровый номер: ')
+cadnum='0523080200:02:001:0200'
+#cadnum = input('Вставьте кадастровый номер: ')
 
 params_point = dict(
     cadnum=cadnum,
 )
 
-params_data = dict(
-    koatuu = cadnum.split(':')[0],
-    zone = cadnum.split(':')[1],
-    quartal = cadnum.split(':')[2],
-    parcel = cadnum.split(':')[3],
-)
-# resp = (json.loads(requests.get(url=url_data, params=params_point).text))
-# print(resp)
-
 resp = (json.loads(requests.get(url=url_point, params=params_point).text)['data'])[0]
 try:
+    params_data = dict(
+        koatuu=cadnum.split(':')[0],
+        zone=cadnum.split(':')[1],
+        quartal=cadnum.split(':')[2],
+        parcel=cadnum.split(':')[3],
+    )
     resp.update((json.loads(requests.get(url=url_data, params=params_data).text)['data'])[0])
+    coordinates.addCoordinates(resp)
     print(resp)
 except:
-    print("Нет данных")
-
+    print("Нет данных по {}".format(cadnum))
