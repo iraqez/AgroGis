93dbbf69d2b9b67797ee5115055926f95c313a64
Iraqez <vladimir.gumenyuk@gmail.com>
2016-07-10 18:56:34 +0300

    1

--- db.py
+++ db.py
@@ -9,12 +9,10 @@ conn = psycopg2.connect("host=agro2012.com.ua dbname=agro2012 user=postgres pass
 cur = conn.cursor()
 
 s1 = datacadnum.params_point.get('cadnum')
-print(type(s1))
-cur.execute('SELECT count(*) FROM cadnum where cadnum = %s', ('0524587000:01:003:0358',))
+cur.execute('SELECT count(*) FROM cadnum where cadnum = %s', (s1,))
 s = cur.fetchone()
-print(s[0])
 
-if s[0] == 0:
+if s[0] == False:
     lat = str(datacadnum.resp.get('lat'))
     lng = str(datacadnum.resp.get('long'))
     cadnum = datacadnum.resp.get('cadnum')
@@ -22,6 +20,7 @@ if s[0] == 0:
 
     cur.execute('INSERT INTO cadnum(geom, lat, lng, cadnum) VALUES (%s, %s, %s, %s)',(geom, lat, lng, cadnum))
     conn.commit()
+    print('Номер {} успешно добавлен!!!'.format(s1) )
 else:
     print('Номер {} существует в базе!!!'.format(s1) )
 
